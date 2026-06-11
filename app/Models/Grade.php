d7a2fcef0f0e38780ae2c448cd267f082f794987
abdrhmn-mmadi <abdrhmn.mmadi@gmail.com>
2026-06-11 12:14:36 +0300

    Create Grade model

---
+++ app/Models/Grade.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Grade extends Model
+{
+    protected $fillable = [
+        'student_id',
+        'course_id',
+        'enrollment_id',
+        'note',
+        'date_notation',
+    ];
+
+    protected $casts = [
+        'note' => 'float',
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function enrollment()
+    {
+        return $this->belongsTo(Enrollment::class);
+    }
+}
